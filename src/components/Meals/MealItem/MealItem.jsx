6db49056aeb78fd59b57e7bf0e056554c674f8e5
K3nny13 <kenneth_cogzell@hotmail.com>
2021-10-18 13:42:40 +0200

    added Input and MealItemForm components

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,4 +1,5 @@
 import styles from './MealItem.module.css'
+import MealItemForm from './MealItemForm'
 
 const MealItem = (props) => {
 
@@ -12,7 +13,7 @@ const MealItem = (props) => {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-          
+          <MealItemForm />
       </div>
     </li>
   )
--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,5 +1,12 @@
-const MealItemForm = props => {
-    return pass
+import styles from './MealItemForm.module.css'
+
+const MealItemForm = (props) => {
+  return (
+    <form className={styles.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  )
 }
 
 export default MealItemForm
